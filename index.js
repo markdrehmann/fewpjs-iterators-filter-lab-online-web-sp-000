f20956f9aceb85be0e2beb642be3791147f413ce
markdrehmann <markdrehmann@gmail.com>
2021-03-24 17:16:03 -0500

    commit tree ac3dfafa2d966ba9e2b9562befec082718092dc0

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(array, searchString) {
+  return array.filter(el => el.toLowerCase() === searchString.toLowerCase())
+}
+
+function fuzzyMatch(array, searchString) {
+  return array.filter(el => el.startsWith(searchString))
+}
+
+function matchName(array, searchString) {
+  return array.filter(el => el.name === searchString)
+}
